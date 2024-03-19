2b04e6d638538e299b16c24872dfaf41c660af3f
frirsta <frirsta.karem@hantverkshjalpenonline.se>
2024-03-19 06:23:01 +0100

    Add NotFound component to handle 404 routes
    - Created NotFound component to display a 404 error message.
    - Added NotFound component to handle routes with paths that do not match any existing routes.

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App.jsx";
 import Context from "./context/Context.jsx";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SignIn from "./pages/Auth/SignIn.jsx";
 import SignUp from "./pages/Auth/SignUp.jsx";
 import Reset from "./pages/Auth/Reset.jsx";
 import Profile from "./pages/profiles/Profile.jsx";
 import NavBar from "./components/navbar/Navbar.jsx";
 import Explore from "./pages/base/Explore.jsx";
-
+import NotFound from "./pages/base/NotFound.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
@@ -22,6 +22,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
           <Route path="/reset" element={<Reset />} />
           <Route path="/explore" element={<Explore />} />
           <Route path="/profile/:id" element={<Profile />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Context>
     </BrowserRouter>
